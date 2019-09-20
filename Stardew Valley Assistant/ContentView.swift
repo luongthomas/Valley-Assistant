98009761ae9ebb02e0fe104820be79c638e8b105
Thomas Luong <Luongt@me.com>
2019-09-19 21:24:16 -0700

    Initial Commit

---
+++ Stardew Valley Assistant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stardew Valley Assistant
+//
+//  Created by Thomas Luong on 9/19/19.
+//  Copyright © 2019 Lithogen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
