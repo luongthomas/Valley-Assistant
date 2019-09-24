cc82686449e87dedca2f5ae233498069aa68cd1a
Thomas Luong <Luongt@me.com>
2019-09-24 11:48:43 -0700

    Add search bar and filtering capability to AllItems view

--- Stardew Valley Assistant/Views/ItemsPage/AllItemsView.swift
+++ Stardew Valley Assistant/Views/ItemsPage/AllItemsView.swift
@@ -9,19 +9,59 @@
 import SwiftUI
 
 struct AllItemsView: View {
+    @State private var searchTerm = ""
+    
     var body: some View {
         VStack {
-            List(items, id: \.id) { item in
-                NavigationLink(destination: ItemDetailView(item: item)) {
-                    Text(item.name)
+            List {
+                SearchBar(text: $searchTerm)
+                
+                ForEach(items.filter {
+                    self.searchTerm.isEmpty ? true : $0.name.localizedCaseInsensitiveContains(self.searchTerm)
+                }, id: \.id) { item in
+                    NavigationLink(destination: ItemDetailView(item: item)) {
+                        Text(item.name)
+                    }
                 }
-            }
-        }.navigationBarTitle("All Items")
+            }.navigationBarTitle("All Items")
+        }
     }
 }
 
 struct AllItemsView_Previews: PreviewProvider {
     static var previews: some View {
         AllItemsView()
+    }
+}
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
     }
 }
