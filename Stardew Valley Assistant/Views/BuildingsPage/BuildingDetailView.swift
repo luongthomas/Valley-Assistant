29532358e1a3bb4655e47e86e7936874789c39cf
Thomas Luong <Luongt@me.com>
2019-10-01 00:04:59 -0700

    Fix weird spacing with Building Detail View

--- Stardew Valley Assistant/Views/BuildingsPage/BuildingDetailView.swift
+++ Stardew Valley Assistant/Views/BuildingsPage/BuildingDetailView.swift
@@ -12,36 +12,34 @@ struct BuildingDetailView: View {
     var building: Building
     
     var body: some View {
-        NavigationView {
-            VStack(alignment: .leading) {
-                Text("Type: \(building.type.rawValue.capitalized)")
-                Text("Hours: \(building.openTime.hour) \(building.openTime.ampm.rawValue.capitalized) - \(building.closeTime.hour) \(building.closeTime.ampm.rawValue.capitalized)")
-                Text("Description: \(building.description)")
-                
-                Spacer()
+        VStack(alignment: .leading) {
+            Text("Type: \(building.type.rawValue.capitalized)")
+            Text("Hours: \(building.openTime.hour) \(building.openTime.ampm.rawValue.capitalized) - \(building.closeTime.hour) \(building.closeTime.ampm.rawValue.capitalized)")
+            Text("Description: \(building.description)")
+            
+            Spacer()
 
-                Text("Occupants:").font(.subheadline)
-                List {
-                    ForEach(building.occupants, id: \.self) { name in
-                        VillagerRow(villager: villagers.first(where: {
-                            $0.name == name
-                        })!)
-                    }
+            Text("Occupants:").font(.subheadline)
+            List {
+                ForEach(building.occupants, id: \.self) { name in
+                    VillagerRow(villager: villagers.first(where: {
+                        $0.name == name
+                    })!)
                 }
-                
-                Divider()
-                
-                Text("Obtainables:").font(.subheadline)
-                List {
-                    ForEach(building.obtainables, id: \.self) { obtainable in
-                        ItemRow(item: items.first(where: {
-                            $0.name == obtainable
-                        })!)
-                    }
-                }
-                
-                .navigationBarTitle("\(building.name)")
             }
-        }.environment(\.horizontalSizeClass, .compact)
+            
+            Divider()
+            
+            Text("Obtainables:").font(.subheadline)
+            List {
+                ForEach(building.obtainables, id: \.self) { obtainable in
+                    ItemRow(item: items.first(where: {
+                        $0.name == obtainable
+                    })!)
+                }
+            }
+
+            .navigationBarTitle("\(building.name)")
+        }
     }
 }
