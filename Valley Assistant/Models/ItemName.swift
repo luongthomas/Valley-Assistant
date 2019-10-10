f1f5a8ee906dacb52280602b42c804d0cbde3a53
Thomas Luong <Luongt@me.com>
2019-10-10 13:59:03 -0700

    Add itemNames for lots more entries

--- Valley Assistant/Models/ItemName.swift
+++ Valley Assistant/Models/ItemName.swift
@@ -9,13 +9,11 @@
 import Foundation
 
 enum ItemName: String, Codable {
-    case daffodil, emerald, aquamarine, poppy, sapphire, ruby, axe, omelette, egg, barn, bigBarn, grassStarter, sugar, wheatFlour, rice, oil, vinegar, basicFertilizer, qualityFertilizer, basicRetainingSoil, qualityRetainingSoil, speedGro, deluxeSpeedGro, wallpaper, flooring, cherrySapling, apricotSapling, orangeSapling, peachSapling, pomegranateSapling, appleSapling, catalogue, bouquet, largePack, deluxePack, sap, anyFish, stone, clay, pineTar, oakResin, coral
+    case daffodil, axe, barn, bigBarn, basicFertilizer, qualityFertilizer, basicRetainingSoil, qualityRetainingSoil, speedGro, deluxeSpeedGro, wallpaper, flooring, cherrySapling, apricotSapling, orangeSapling, peachSapling, pomegranateSapling, appleSapling, catalogue, bouquet, largePack, deluxePack, sap, anyFish, stone, clay, pineTar, oakResin, coral
     
-    case parsnipSeeds, beanStarter, cauliflowerSeeds, potatoSeeds, tulipBulb, kaleSeeds, jazzSeeds, garlicSeeds
     
-    case melonSeeds, tomatoSeeds, blueberrySeeds, pepperSeeds, wheatSeeds, radishSeeds, poppySeeds, spangleSeeds, hopsStarter, cornSeeds, sunflowerSeeds, redCabbageSeeds
-    
-    case eggplantSeeds, pumpkinSeeds, bokChoySeeds, yamSeeds, cranberrySeeds, fairySeeds, amaranthSeeds, grapeStarter, artichokeSeeds
+    // Seeds
+    case parsnipSeeds, beanStarter, cauliflowerSeeds, potatoSeeds, tulipBulb, kaleSeeds, jazzSeeds, garlicSeeds, melonSeeds, tomatoSeeds, blueberrySeeds, pepperSeeds, wheatSeeds, radishSeeds, poppySeeds, spangleSeeds, hopsStarter, cornSeeds, sunflowerSeeds, redCabbageSeeds, eggplantSeeds, pumpkinSeeds, bokChoySeeds, yamSeeds, cranberrySeeds, fairySeeds, amaranthSeeds, grapeStarter, artichokeSeeds, grassStarter
     
     case energyTonic, muscleRemedy
     
@@ -25,10 +23,49 @@ enum ItemName: String, Codable {
     
     case jojaCola, jColaLight, jojaWallpaper, minecarts, panning, bridge, greenhouse, bus
     
-    case salad, bread, spaghetti, pizza, coffee, hashbrownsRecipe, omeletRecipe, pancakesRecipe, breadRecipe, tortillaRecipe, pizzaRecipe, makiRollRecipe
+    case hashbrownsRecipe, omeletRecipe, pancakesRecipe, breadRecipe, tortillaRecipe, pizzaRecipe, makiRollRecipe
     
-    case leek, dandelion, cheese, tomato, milk, largeMilk
+    case goldenPumpkin, pearl, rabbitsFoot
+
     
-    case goldenPumpkin, pearl, prismaticShard, rabbitsFoot, completeBreakfast, salmonDinner, friedEgg, pancakes, hashbrowns, potato, corn, sunflower, salmon, amaranth, kale
+    // Recipes
+    case friedEgg, omelet, salad, bakedFish, parsnipSoup, vegetableMedley, completeBreakfast, friedCalamari, strangeBun, luckyLunch, friedMushroom, pizza, beanHotpot, glazedYams, carpSurprise, hashbrowns, pancakes, salmonDinner, fishTaco, crispyBass, pepperPoppers, breat, tomKhaSoup, troupSOup, chocolateCake, pinkCake, rhubarbPie, cookie, spaghetti, friedEel, spicyEel, sashimi, makiRoll, tortilla, redPlate, eggParmesan, ricePudding, iceCream, blueberryTart, autumnsBounty, pumpkinSoup, superMeal, cranberrySauce, stuffing, farmersLunch, survivalBurger, dishOTheSea, minersTreat, rootsPlatter, algaeSoup, paleBroth, plumPudding, artichokeDip, stirFry, roastedHazelnuts, pumpkinPie, radishSalad, fruitSalad, blackberryCobbler, cranberryCandy, bruschetta, coleslaw, fiddleheadRisotto, poppyseedMuffin, chowder, lobsterBisque, escargot, fishStew, mapleBar, crabCakes, fieldSnack, lifeElixir, oilOfGarlic
+    
+    // Crops
+    case parsnip, kale, potato, blueJazz, greenBean, cauliflower, rhubarb, garlic, hotPepper, radish, poppy, tomato, melon, blueberry, corn, redCabage, bokChoy, eggplant, amaranth, cranberries, yam, pumpkin, beet, artichoke
+    
+    // Foraged Goods
+    case blackberry, caveCarrot, commonMushroom, dandelion, coconut, fiddleheadFern, hazelnut, leek, morel, wildPlum, winterRoot
+    
+    // Tree Products
+    case apple, apricot, mapleSyrup
+    
+    // Animal Products
+    case egg, milk, largeMilk, duckFeather, dinosaurEgg, duckEgg, goatMilk, largeEgg, largeGoatMilk, slime, slimeBall, truffle, voidEgg, wool
+    
+    // Artisan Goods
+    case honey, wine, paleAle, beer, mead, cheese, goatCheese, juice, coffee, cloth, mayonnaise, duckMayonnaise, voidMayonnaise, truffleOil, oil, pickles, jelly
+    
+    // Fish
+    case tuna, sardine, bream, largemouthBass, rainbowTrout, salmon, carp, sunfish, eel, squid, seaCucumber, seaweed, greenAlgae, whiteAlgae
+    
+    // Crab Pot items
+    case lobster, clam, crayfish, crab, mussel, shrimp, snail, periwinkle
+    
+    // General Store Items
+    case sugar, wheatFlour, rice
+    
+    // Gems
+    case emerald, aquamarine, ruby, amethyst, topaz, jade, diamond, prismaticShard
+    
+    // Geode Minerals
+    case tigerseye, opal, fireOpal, alamite, bixite, baryte, aerinite, calcite, dolomite, esperite, fluorapatite, geminite, helvite, jamborite, jagoite, kyanite, lunarite, malachite, neptunite, lemonStone, nekoite, orpiment, petrifiedSlime, thunderEgg, pyrite, oceanStone, ghostCrystal, jasper, celestine, marble, sandstone, granite, basalt, limestone, soapstone, hematite, mudstone, obsidian, slate, fairyStone, starShards
+    
+    // Geodes
+    case geode, grozenGeode, magmanGeode, omniGeode
+    
+    // Forged Minerals
+    case earthCrystal, fireQuartz, frozenTear, quartz
+    
     
 }
