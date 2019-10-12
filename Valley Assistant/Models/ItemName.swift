daf2328df423e91539ba98ebdb0df422d7a48bd9
Thomas Luong <Luongt@me.com>
2019-10-11 22:48:50 -0700

    Finished minerals and geodes

--- Valley Assistant/Models/ItemName.swift
+++ Valley Assistant/Models/ItemName.swift
@@ -26,7 +26,7 @@ enum ItemName: String, Codable {
     case pickles, jelly
     
     // Seeds
-    case parsnipSeeds, beanStarter, cauliflowerSeeds, potatoSeeds, tulipBulb, kaleSeeds, jazzSeeds, garlicSeeds, melonSeeds, tomatoSeeds, blueberrySeeds, pepperSeeds, wheatSeeds, radishSeeds, poppySeeds, spangleSeeds, hopsStarter, cornSeeds, sunflowerSeeds, redCabbageSeeds, eggplantSeeds, pumpkinSeeds, bokChoySeeds, yamSeeds, cranberrySeeds, fairySeeds, amaranthSeeds, grapeStarter, artichokeSeeds, grassStarter, fallSeeds, winterSeeds, summerSeeds, springSeeds, cactusSeeds, mapleSeed
+    case parsnipSeeds, beanStarter, cauliflowerSeeds, potatoSeeds, tulipBulb, kaleSeeds, jazzSeeds, garlicSeeds, melonSeeds, tomatoSeeds, blueberrySeeds, pepperSeeds, wheatSeeds, radishSeeds, poppySeeds, spangleSeeds, hopsStarter, cornSeeds, sunflowerSeeds, redCabbageSeeds, eggplantSeeds, pumpkinSeeds, bokChoySeeds, yamSeeds, cranberrySeeds, fairySeeds, amaranthSeeds, grapeStarter, artichokeSeeds, grassStarter, fallSeeds, winterSeeds, summerSeeds, springSeeds, cactusSeeds, mapleSeed, rareSeed
     
     
     
@@ -34,7 +34,7 @@ enum ItemName: String, Codable {
     case friedEgg, omelet, salad, bakedFish, parsnipSoup, vegetableMedley, completeBreakfast, friedCalamari, strangeBun, luckyLunch, friedMushroom, pizza, beanHotpot, glazedYams, carpSurprise, hashbrowns, pancakes, salmonDinner, fishTaco, crispyBass, pepperPoppers, breat, tomKhaSoup, troupSOup, chocolateCake, pinkCake, rhubarbPie, cookie, spaghetti, friedEel, spicyEel, sashimi, makiRoll, tortilla, redPlate, eggParmesan, ricePudding, iceCream, blueberryTart, autumnsBounty, pumpkinSoup, superMeal, cranberrySauce, stuffing, farmersLunch, survivalBurger, dishOTheSea, minersTreat, rootsPlatter, algaeSoup, paleBroth, plumPudding, artichokeDip, stirFry, roastedHazelnuts, pumpkinPie, radishSalad, fruitSalad, blackberryCobbler, cranberryCandy, bruschetta, coleslaw, fiddleheadRisotto, poppyseedMuffin, chowder, lobsterBisque, escargot, fishStew, mapleBar, crabCakes, fieldSnack, lifeElixir, oilOfGarlic
     
     // Crops
-    case parsnip, kale, potato, blueJazz, greenBean, cauliflower, rhubarb, garlic, hotPepper, radish, poppy, tomato, melon, blueberry, corn, redCabage, bokChoy, eggplant, amaranth, cranberries, yam, pumpkin, beet, artichoke
+    case parsnip, kale, potato, blueJazz, greenBean, cauliflower, rhubarb, garlic, hotPepper, radish, poppy, tomato, melon, blueberry, corn, redCabage, bokChoy, eggplant, amaranth, cranberries, yam, pumpkin, beet, artichoke, sweetGemBerry
     
     // Foraged Goods
     case blackberry, caveCarrot, commonMushroom, dandelion, coconut, fiddleheadFern, hazelnut, leek, morel, wildPlum, winterRoot, nautilusShell, coral, seaUrchin, rainbowShell, crocus, holly, crystalFruit, snowYam, chanterelle, grape, spiceBerry, sweetPea, sap, wildHorseradish, springOnion, salmonBerry, cactusFruit
@@ -64,7 +64,7 @@ enum ItemName: String, Codable {
     case tigerseye, opal, fireOpal, alamite, bixite, baryte, aerinite, calcite, dolomite, esperite, fluorapatite, geminite, helvite, jamborite, jagoite, kyanite, lunarite, malachite, neptunite, lemonStone, nekoite, orpiment, petrifiedSlime, thunderEgg, pyrite, oceanStone, ghostCrystal, jasper, celestine, marble, sandstone, granite, basalt, limestone, soapstone, hematite, mudstone, obsidian, slate, fairyStone, starShards
     
     // Geodes
-    case geode, grozenGeode, magmanGeode, omniGeode
+    case geode, frozenGeode, magmaGeode, omniGeode
     
     // Forged Minerals
     case earthCrystal, fireQuartz, frozenTear, quartz
